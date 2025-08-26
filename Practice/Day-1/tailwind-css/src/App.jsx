26d0ebb021c12b091c9130b9ec10af849d04cb79
josh-a-bant <jashobantbesra@gmail.com>
2025-08-27 04:33:45 +0530

    tailwind intro

---
+++ Practice/Day-1/tailwind-css/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-xl bg-red-400 text-rose-800 m-2">One</div>
+      <div className="text-xl border-4 border-slate-600 text-slate-800 m-2">
+        Two
+      </div>
+      <div className="text-3xl text-blue-800 bg-emerald-200 border-4 border-emerald-800 m-2">
+        Three
+      </div>
+      <div className="text-xl text-purple-800 border-4 border-sky-400 m-2">
+        Four
+      </div>
+      <div className="text-xl text-purple-800 border-2 border-l-8 border-l-purple-400 bg-violet-200 m-2">
+        Five
+      </div>
+    </>
+  );
+}
+
+export default App;
