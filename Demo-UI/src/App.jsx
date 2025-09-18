f96317d3c43892258121e9089e414484abe347f8
josh-a-bant <jashobantbesra@gmail.com>
2025-09-18 22:49:50 +0530

    add new files

---
+++ Demo-UI/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { TextHoverEffectDemo } from "./TextHoverEffectDemo";
+
+function App() {
+  return (
+    <div className="bg-neutral-900 h-screen">
+      <TextHoverEffectDemo />
+    </div>
+  );
+}
+
+export default App;
---
+++ Demo-UI/src/TextHoverEffectDemo.jsx
@@ -0,0 +1,9 @@
+import { TextHoverEffect } from "./components/text-hover-effect";
+
+export function TextHoverEffectDemo() {
+  return (
+    <div className="h-[40rem] flex items-center justify-center">
+      <TextHoverEffect text="jk" />
+    </div>
+  );
+}
