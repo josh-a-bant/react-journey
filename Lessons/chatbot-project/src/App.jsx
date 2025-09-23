3cc4319a0ce7d35a7232ea8d468534f4b09fcbe1
josh-a-bant <jashobantbesra@gmail.com>
2025-09-24 01:10:23 +0530

    modify chatbox project

--- Lessons/chatbot-project/src/App.jsx
+++ Lessons/chatbot-project/src/App.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import "./App.css";
+import ChatMessages from "./components/ChatMessages";
 import ChatbotHeading from "./components/ChatbotHeading";
-import ChatbotMessages from "./components/ChatbotMessages";
 
 function App() {
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: "hello chatbot",
+      sender: "user",
+      id: "id1",
+    },
+    {
+      message: "Hello! How can I help you?",
+      sender: "robot",
+      id: "id2",
+    },
+    {
+      message: "can you get me todays date?",
+      sender: "user",
+      id: "id3",
+    },
+    {
+      message: "Today is September 27",
+      sender: "robot",
+      id: "id4",
+    },
+  ]);
+
   return (
-    <>
-      <ChatbotHeading />
-      <ChatbotMessages message="Hello chatbot" sender="user" />
-      <ChatbotMessages message="Hello! How can i help you" sender="robot" />
-      <ChatbotMessages message="can you get me today date ?" sender="user" />
-      <ChatbotMessages message="Today is september 20" sender="robot" />
-    </>
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatbotHeading
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
   );
 }
 
--- Lessons/chatbot-project/src/components/ChatbotHeading.jsx
+++ Lessons/chatbot-project/src/components/ChatbotHeading.jsx
@@ -1,9 +1,71 @@
-const ChatbotHeading = () => {
+import { useState } from "react";
+import { Chatbot } from "supersimpledev";
+import "./ChatInput.css";
+import ChatMessages from "./ChatMessages";
+
+const ChatbotHeading = ({ chatMessages, setChatMessages }) => {
+  // function handleKeyDown(event) {
+  //   if (event.key === "Enter") {
+  //     console.log("hi");
+  //   } else if (event.key === "Escape") {
+  //     console.log("hello");
+  //   }
+  // }
+  // return (
+  //   <>
+  //     <input
+  //       placeholder="send a message to chatbox"
+  //       size={30}
+  //       onKeyDown={handleKeyDown}
+  //     />
+  //     <button>send</button>
+  //   </>
+  // );
+
+  const [inputText, setInputText] = useState("");
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  function sendMessage() {
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: "user",
+        id: crypto.randomUUID(),
+      },
+    ];
+
+    setChatMessages(newChatMessages);
+
+    const response = Chatbot.getResponse(inputText);
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+
+    setInputText("");
+  }
+
   return (
-    <>
-      <input placeholder="send a message to chatbox" size={30} />
-      <button>send</button>
-    </>
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        value={inputText}
+        className="chat-input"
+      />
+      <button onClick={sendMessage} className="send-button">
+        Send
+      </button>
+    </div>
   );
 };
 
