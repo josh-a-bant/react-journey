fc4222213b8f4477de005df9f1987f802acb958a
josh-a-bant <jashobantbesra@gmail.com>
2025-10-06 00:50:30 +0530

    error handling

--- forms/src/components/YouTubeForm.jsx
+++ forms/src/components/YouTubeForm.jsx
@@ -1,23 +1,55 @@
 import { useForm } from "react-hook-form";
 
 const YouTubeForm = () => {
-  const form = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  const onSubmit = (data) => {
+    console.log("form submitted", data);
+  };
   return (
     <div className="max-w-md mx-auto mt-10">
-      <form action="" className="flex flex-col">
-        <label htmlFor="username">Usernam</label>
-        <input type="text" name="username" className="border rounded-md" />
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+        <label htmlFor="username">
+          Usernam
+          <span className="text-red-500 text-xs ml-2">
+            {errors.username?.message}
+          </span>
+        </label>
+
+        <input
+          type="text"
+          className="border rounded-md"
+          {...register("username", { required: "username is required" })}
+        />
 
         <label htmlFor="email" className="mt-5">
           E-mail
         </label>
-        <input type="email" name="email" className="border rounded-md" />
+        <input
+          type="email"
+          className="border rounded-md"
+          {...register("email", {
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "invalid email",
+            },
+          })}
+        />
+        <p className="text-red-500 text-sm">{errors.email?.message}</p>
 
         <label htmlFor="channel" className="mt-5">
           Channel
         </label>
-        <input type="text" name="channel" className="border rounded-md" />
+        <input
+          type="text"
+          className="border rounded-md"
+          {...register("channel", { required: "channel name is required" })}
+        />
+        <p className="text-red-500 text-sm">{errors.channel?.message}</p>
 
         <input type="submit" className="border mt-5 rounded-md " />
       </form>
