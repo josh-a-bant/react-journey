4640983ea99b7e32ce2fcf2713a995bd8946beeb
josh-a-bant <jashobantbesra@gmail.com>
2025-10-16 02:18:56 +0530

    implement react router

--- Practice/Day-6/react-router/src/main.jsx
+++ Practice/Day-6/react-router/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <RouterProvider router={router} />
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
