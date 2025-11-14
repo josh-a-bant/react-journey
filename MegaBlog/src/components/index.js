771606d1d82b89351dd9170a9d52a581b26dc90d
josh-a-bant <jashobantbesra@gmail.com>
2025-11-15 00:59:37 +0530

    react hook form

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./footer/Footer.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
