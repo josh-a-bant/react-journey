3a835ef363f300de31fc82d0b138003620f59268
josh-a-bant <jashobantbesra@gmail.com>
2025-11-15 03:39:10 +0530

    components

--- core-concepts/src/App.jsx
+++ core-concepts/src/App.jsx
@@ -1,29 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+function Welcome() {
+  return <h2>Lorem ipsum dolor sit amet.</h2>;
+}
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Core Concepts</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Jay Jagannath</p>
-      </div>
-      <p className="read-the-docs">JK I Love You</p>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <Welcome />
     </>
   );
 }
