d4303163c14487dd5294df4ce0bdfc296a667bf5
josh-a-bant <jashobantbesra@gmail.com>
2025-11-21 00:44:06 +0530

    complete this project

--- prop-mini-project/src/App.jsx
+++ prop-mini-project/src/App.jsx
@@ -116,7 +116,20 @@ function App() {
 
   return (
     <div className="container">
-      <Card />
+      {jobOpenings.map((job, index) => (
+        <div key={index}>
+          <Card
+            brandLogo={job.brandLogo}
+            companyName={job.companyName}
+            datePosted={job.datePosted}
+            post={job.post}
+            tag1={job.tag1}
+            tag2={job.tag2}
+            pay={job.pay}
+            location={job.location}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- prop-mini-project/src/components/Card.jsx
+++ prop-mini-project/src/components/Card.jsx
@@ -1,15 +1,12 @@
 import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <div>
         <div className="header">
           <div>
-            <img
-              src="https://substackcdn.com/image/fetch/$s_!G1lk!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg"
-              alt=""
-            />
+            <img src={props.brandLogo} alt="" />
           </div>
           <div>
             <button>
@@ -23,22 +20,22 @@ const Card = () => {
         <div className="main">
           <div>
             <h3>
-              Airbnb <span>5 days ago</span>
+              {props.companyName} <span>{props.datePosted}</span>
             </h3>
           </div>
           <div>
-            <h1>Junior UI/UX Designer</h1>
+            <h1>{props.post}</h1>
           </div>
           <div className="tag">
-            <div className="tags">Full Time</div>
-            <div className="tags">Remote</div>
+            <div className="tags">{props.tag1}</div>
+            <div className="tags">{props.tag2}</div>
           </div>
         </div>
       </div>
       <div className="footer">
         <div>
-          <h2>$100/hr</h2>
-          <div className="location">Delhi, India</div>
+          <h2>{props.pay}</h2>
+          <div className="location">{props.location}</div>
         </div>
         <div>
           <button>Apply now</button>
