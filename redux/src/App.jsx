764226ac8bef072eca40bf64919a713785b74eb5
josh-a-bant <jashobantbesra@gmail.com>
2026-02-09 12:48:48 +0530

    use redux for counter

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,7 +1,34 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
-  return <>JK - Radha</>;
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+
+  return (
+    <>
+      <div>
+        <h1 className="text-6xl mb-4">{count}</h1>
+        <button
+          className="border mx-2 px-4 py-2 rounded-md bg-neutral-600 text-3xl text-white"
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          increment
+        </button>
+        <button
+          className="border mx-2 px-4 py-2 rounded-md bg-neutral-600 text-3xl text-white"
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          decrement
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
