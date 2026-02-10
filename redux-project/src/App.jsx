07ec4c23b312c0ee741e5cee60440575f0900efd
josh-a-bant <jashobantbesra@gmail.com>
2026-02-10 19:55:26 +0530

    get photos, videos, Gif from the API

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { fetchGIF, fetchImage, fetchVideos } from "./api/mediaApi";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-screen w-full bg-neutral-600 text-white">
+      <button
+        className="m-4 border px-4 py-2"
+        onClick={() => {
+          fetchImage("cat");
+        }}
+      >
+        Get Photos
+      </button>
+      <button
+        className="m-4 border px-4 py-2"
+        onClick={() => {
+          fetchVideos("dog");
+        }}
+      >
+        Get videos
+      </button>
+
+      <button
+        className="m-4 border px-4 py-2"
+        onClick={() => {
+          fetchGIF("dog");
+        }}
+      >
+        Get GIF
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ redux-project/src/api/mediaApi.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+const TENOR_KEY = import.meta.env.VITE_TENOR_KEY;
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
+
+export async function fetchImage(query, page = 1, per_page = 20) {
+  const res = await axios.get("https://api.unsplash.com/search/photos", {
+    params: { query, page, per_page },
+    headers: { Authorization: `Client-ID ${UNSPLASH_KEY}` },
+  });
+  console.log(res.data.results);
+}
+
+export async function fetchVideos(query) {
+  const res = await axios.get("https://api.pexels.com/videos/search", {
+    params: { query },
+    headers: { Authorization: PEXELS_KEY },
+  });
+
+  console.log(res.data.videos);
+}
+
+export async function fetchGIF(query, limit = 20) {
+  const res = await axios.get("https://tenor.googleapis.com/v2/search", {
+    params: { q: query, key: TENOR_KEY, client_key: "my_react_app", limit },
+  });
+
+  console.log(res.data.results);
+}
